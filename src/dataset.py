bee7c9fc67202316e83b548cc6712df1a83124f4
Colab6984 <122926213+Colab6984@users.noreply.github.com>
2023-01-17 23:59:01 +0000

    Added filecache module to manage caching of the processed dataset

---
+++ src/dataset.py
@@ -0,0 +1,48 @@
+from binascii import Error
+from PIL import Image
+import numpy as np
+import pickle
+import os
+import hashlib
+import inspect
+from filecache import filecache
+
+
+@filecache
+def load_images(file):
+  with open(file, "rb") as f:
+    read_bytes = lambda byte_size:int.from_bytes(f.read(byte_size), byteorder="big")
+    f.read(3) #Jump over first 3 bytes
+    dimensions = read_bytes(1)
+    images_amount = read_bytes(4)
+    rows_amount = read_bytes(4)
+    colums_amount = read_bytes(4)
+
+    print("dimensions:", dimensions)
+    print("shape:", (images_amount, rows_amount, colums_amount))
+
+    images = []
+
+    for i in range(images_amount):
+      image = []
+      for x in range(rows_amount):
+        for y in range(colums_amount):
+          image.append(read_bytes(1))
+      images.append(image)
+
+    images = np.array(images, dtype=np.uint8).reshape((images_amount, 28,28))
+  return images
+
+def image_to_file(image, filename):
+  image = Image.fromarray(image, 'L')
+  image.save(filename)
+  image.show()
+
+
+def load_labels(file):
+  pass
+
+
+
+
+
--- src/main.py
+++ src/main.py
@@ -7,8 +7,12 @@ import numpy as np
 import os 
 
 
+
 images = dataset.load_images("datasets/t10k-images-idx3-ubyte")
-os.makedirs("images")
+try:
+    os.makedirs("images")
+except:
+    pass
 for i in range(9):
   image = images[i]
   dataset.image_to_file(image, f"images/image{i}.png")
