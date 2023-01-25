1dc2734621ec7408741797d3b79a22d89b4b53c2
ChristianGynnild <christianwgynnild@protonmail.com>
2023-01-26 00:58:13 +0100

    Added a function and cli command to predict which number a image represents

--- src/cli.py
+++ src/cli.py
@@ -14,6 +14,11 @@ def test():
 def train():
     model.train_and_test()
 
+@cli.command()
+@click.argument('image_path')
+def predict(image_path):
+    model.predict(image_path)
+
 @test.command()
 def load_label():
     dataset.load_labels("datasets/t10k-labels-idx1-ubyte")
--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 import dataset
 import numpy as np
+from PIL import Image
 
 device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 print(f"Using {device} device")
@@ -100,3 +101,41 @@ def train_and_test(epochs=5):
 
     torch.save(model.state_dict(), MODEL_WEIGHTS_PATH)
     print(f"Saved PyTorch Model State to {MODEL_WEIGHTS_PATH}")
+
+def predict(image_path):
+
+    image = Image.open(image_path)
+    print("Opened:", image_path)
+    print("shape:",image.size)
+    width_height_difference = image.width-image.height
+
+    if width_height_difference<0:
+        y_shift=int((image.height-image.width)/2)
+        box = (0, y_shift, image.width, y_shift+image.width)
+        print("Image is not rectangular, cropping the height")
+        image = image.crop(box)
+        print("Cropped size:", image.size)
+    if width_height_difference>0:
+        x_shift=int((image.width-image.height)/2)
+        box = (x_shift, 0, x_shift+image.height, image.height)
+        print("Image is not rectangular, cropping the width")
+        image = image.crop(box)
+        print("Cropped size:", image.size)
+
+    size = 28, 28
+    if image.size != size:
+        image.thumbnail(size, Image.Resampling.LANCZOS)
+        print("Downsampled image to:",image.size)
+
+    image = image.convert('L') # Turn the picture grayscale
+
+    array = np.array(image, dtype=np.float32).reshape((1,28,28))
+
+
+    model = NeuralNetwork().to(device)
+    try:model.load_state_dict(torch.load(MODEL_WEIGHTS_PATH))
+    except Exception:pass
+    model.eval()
+    prediction = model(torch.from_numpy(array))
+    print(int(prediction[0].argmax(0)))
+    image.save("twst.jpeg", "png")
