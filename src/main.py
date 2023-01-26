afe68cd212b57100fd332d65d258a93291924e83
ChristianGynnild <christianwgynnild@protonmail.com>
2023-01-26 11:26:16 +0100

    Cleaned up main.py

--- src/main.py
+++ src/main.py
@@ -2,12 +2,3 @@
 from cli import cli
 
 cli()
-
-# images = dataset.load_images("datasets/t10k-images-idx3-ubyte")
-# try:
-#     os.makedirs("images")
-# except:
-#     pass
-# for i in range(9):
-#   image = images[i]
-#   dataset.image_to_file(image, f"images/image{i}.png")
