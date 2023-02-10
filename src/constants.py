dda01a9fa9f889ff0e74de4b116cf426cd84229d
Colab6984 <122926213+Colab6984@users.noreply.github.com>
2023-02-10 17:50:00 +0000

    Made path constants relative to the project folder path

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,9 @@
-MODEL_WEIGHTS_PATH = "model_weights"
-TRAINING_IMAGES_PATH = "datasets/train-images-idx3-ubyte"
-TRAINING_LABELS_PATH = "datasets/train-labels-idx1-ubyte"
-TEST_IMAGES_PATH = "datasets/t10k-images-idx3-ubyte"
-TEST_LABELS_PATH = "datasets/t10k-labels-idx1-ubyte"
+import os as _os
+from pathlib import Path
+
+PROJECT_FOLDER_PATH = Path(_os.path.abspath(__file__)).parent.parent.absolute()
+MODEL_WEIGHTS_PATH = _os.path.join(PROJECT_FOLDER_PATH, "model_weights")
+TRAINING_IMAGES_PATH = _os.path.join(PROJECT_FOLDER_PATH, "datasets/train-images-idx3-ubyte")
+TRAINING_LABELS_PATH = _os.path.join(PROJECT_FOLDER_PATH, "datasets/train-labels-idx1-ubyte")
+TEST_IMAGES_PATH = _os.path.join(PROJECT_FOLDER_PATH, "datasets/t10k-images-idx3-ubyte")
+TEST_LABELS_PATH = _os.path.join(PROJECT_FOLDER_PATH, "datasets/t10k-labels-idx1-ubyte")
