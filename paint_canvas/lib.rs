ce537a41df951b751a60d19aca6a427f28ff0b9d
ChristianGynnild <>
2023-03-13 01:22:46 +0100

    Turned resize into a closure and added color to the triangle

--- paint_canvas/lib.rs
+++ paint_canvas/lib.rs
@@ -1,6 +1,7 @@
-
 #[cfg(target_arch="wasm32")]
 use wasm_bindgen::prelude::*;
+
+use wgpu::util::DeviceExt;
 
 use winit::{
     event::*,
@@ -8,7 +9,46 @@ use winit::{
     window::WindowBuilder,
 };
 
-fn render(device:&wgpu::Device, queue:&wgpu::Queue, surface:&wgpu::Surface)-> Result<(), wgpu::SurfaceError>{
+
+// lib.rs
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
+    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+];
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                }
+            ]
+        }
+    }
+}
+
+
+
+fn render(device:&wgpu::Device, queue:&wgpu::Queue, render_pipeline:&wgpu::RenderPipeline, surface:&wgpu::Surface, vertex_buffer:&wgpu::Buffer)-> Result<(), wgpu::SurfaceError>{
     let output = surface.get_current_texture()?;
     let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
         let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
@@ -34,6 +74,37 @@ fn render(device:&wgpu::Device, queue:&wgpu::Queue, surface:&wgpu::Surface)-> Re
         });
     }
 
+    {
+        // 1.
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[
+                // This is what @location(0) in the fragment shader targets
+                Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(
+                            wgpu::Color {
+                                r: 0.1,
+                                g: 0.2,
+                                b: 0.3,
+                                a: 1.0,
+                            }
+                        ),
+                        store: true,
+                    }
+                })
+            ],
+            depth_stencil_attachment: None,
+        });
+    
+        // NEW!
+        render_pass.set_pipeline(&render_pipeline); // 2.
+        render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
+        
+        render_pass.draw(0..(VERTICES.len() as u32), 0..1);
+    }
     // submit will accept anything that implements IntoIter
     queue.submit(std::iter::once(encoder.finish()));
     output.present();
@@ -43,29 +114,6 @@ fn render(device:&wgpu::Device, queue:&wgpu::Queue, surface:&wgpu::Surface)-> Re
 
 
 
-fn resize(adapter:&wgpu::Adapter, device:&wgpu::Device, surface:&wgpu::Surface, new_size: winit::dpi::PhysicalSize<u32>){
-    if new_size.width > 0 && new_size.height > 0 {
-        let surface_caps = surface.get_capabilities(&adapter);
-        // Shader code in this tutorial assumes an sRGB surface texture. Using a different
-        // one will result all the colors coming out darker. If you want to support non
-        // sRGB surfaces, you'll need to account for that when drawing to the frame.
-        let surface_format = surface_caps.formats.iter()
-            .copied()
-            .filter(|f| f.describe().srgb)
-            .next()
-            .unwrap_or(surface_caps.formats[0]);
-        let config = wgpu::SurfaceConfiguration {
-            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: surface_format,
-            width: new_size.width,
-            height: new_size.height,
-            present_mode: surface_caps.present_modes[0],
-            alpha_mode: surface_caps.alpha_modes[0],
-            view_formats: vec![],
-        };
-        surface.configure(&device, &config);
-    }
-}
 
 #[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
 pub async fn run() {
@@ -82,13 +130,12 @@ pub async fn run() {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-
     // Add canvas to html
     // Winit prevents sizing with CSS, so we have to set
     // the size manually when on web.
     #[cfg(target_arch = "wasm32")] {
     use winit::dpi::PhysicalSize;
-    window.set_inner_size(PhysicalSize::new(450, 450));
+    window.set_inner_size(PhysicalSize::new(1200, 1000));
     
     use winit::platform::web::WindowExtWebSys;
     web_sys::window()
@@ -146,20 +193,31 @@ pub async fn run() {
         push_constant_ranges: &[],
     });
 
-
+    let surface_caps = surface.get_capabilities(&adapter);
+    // Shader code in this tutorial assumes an sRGB surface texture. Using a different
+    // one will result all the colors coming out darker. If you want to support non
+    // sRGB surfaces, you'll need to account for that when drawing to the frame.
+    let surface_format = surface_caps.formats.iter()
+        .copied()
+        .filter(|f| f.describe().srgb)
+        .next()
+        .unwrap_or(surface_caps.formats[0]);
+    
     let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
         label: Some("Render Pipeline"),
         layout: Some(&render_pipeline_layout),
         vertex: wgpu::VertexState {
             module: &shader,
             entry_point: "vs_main", // 1.
-            buffers: &[], // 2.
+            buffers: &[
+                Vertex::desc(),
+            ], // 2.
         },
         fragment: Some(wgpu::FragmentState { // 3.
             module: &shader,
             entry_point: "fs_main",
             targets: &[Some(wgpu::ColorTargetState { // 4.
-                format: surface.format,
+                format: surface_format,
                 blend: Some(wgpu::BlendState::REPLACE),
                 write_mask: wgpu::ColorWrites::ALL,
             })],
@@ -185,11 +243,53 @@ pub async fn run() {
         multiview: None, // 5.
     });
 
-    
 
-    
+    let resize = move |device:&wgpu::Device, surface:&wgpu::Surface, new_size:winit::dpi::PhysicalSize<u32>|{
+        if new_size.width > 0 && new_size.height > 0 {
+            let config = wgpu::SurfaceConfiguration {
+                usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                format: surface_format,
+                width: new_size.width,
+                height: new_size.height,
+                present_mode: surface_caps.present_modes[0],
+                alpha_mode: surface_caps.alpha_modes[0],
+                view_formats: vec![],
+            };
+            surface.configure(&device, &config);
+        }
+    };
 
-    resize(&adapter, &device, &surface, winit::dpi::PhysicalSize{width:1000, height:500});
+    let surface_caps = surface.get_capabilities(&adapter);
+
+    let value = surface_caps.present_modes[0];
+
+    resize(&device, &surface, winit::dpi::PhysicalSize{width:1200, height:1000});
+
+
+    let vertex_buffer = device.create_buffer_init(
+        &wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(VERTICES),
+            usage: wgpu::BufferUsages::VERTEX,
+        }
+    );
+
+    wgpu::VertexBufferLayout {
+        array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress, // 1.
+        step_mode: wgpu::VertexStepMode::Vertex, // 2.
+        attributes: &[ // 3.
+            wgpu::VertexAttribute {
+                offset: 0, // 4.
+                shader_location: 0, // 5.
+                format: wgpu::VertexFormat::Float32x3, // 6.
+            },
+            wgpu::VertexAttribute {
+                offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                shader_location: 1,
+                format: wgpu::VertexFormat::Float32x3,
+            }
+        ]
+    };
 
     event_loop.run(move |event, _, control_flow| {
         match event {
@@ -209,19 +309,19 @@ pub async fn run() {
                         ..
                     } => *control_flow = ControlFlow::Exit,
                     WindowEvent::Resized(physical_size) => {
-                        resize(&adapter, &device, &surface, *physical_size);
+                        resize(&device, &surface, winit::dpi::PhysicalSize{width:1200, height:1000});
                     }
                     WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                        resize(&adapter, &device, &surface, **new_inner_size);
+                        resize(&device, &surface, winit::dpi::PhysicalSize{width:1200, height:1000});
                     }
                     _ => {}
                 }
             }
             Event::RedrawRequested(window_id) if window_id == window.id() => {
-                match render(&device, &queue, &surface) {
+                match render(&device, &queue, &render_pipeline, &surface, &vertex_buffer) {
                     Ok(_) => {}
                     // Reconfigure the surface if lost
-                    Err(wgpu::SurfaceError::Lost) => resize(&adapter, &device, &surface,window_size),
+                    Err(wgpu::SurfaceError::Lost) => resize(&device, &surface, winit::dpi::PhysicalSize{width:1200, height:1000}),
                     // The system is out of memory, we should probably quit
                     Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
                     // All other errors (Outdated, Timeout) should be resolved by the next frame
