2e0a0849ccca35f7179e87becfd791b13d7c7981
ChristianGynnild <>
2023-03-29 19:54:15 +0200

    Succesfully sent drawn image from frontend to backend

--- frontend/public/predict_request_worker.js
+++ frontend/public/predict_request_worker.js
@@ -2,7 +2,12 @@ onmessage = async (e) => {
     console.log("Message received from main script");
     const workerResult = `Result: ${e.data}`;
     console.log("Posting message back to main script");
-    let data = JSON.stringify({"pixels":e.data.pixels});
+    let data = JSON.stringify(
+      {"pixels":e.data.pixels, 
+      "channels_amount":4,
+      "columns_amount":500,
+      "rows_amount":500,
+    });
     console.log("Proccesed json")
 
     await fetch("/predict", {
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, current_app, request
 import json
+from PIL import Image
+import PIL
+import numpy as np
 
 app = Flask(__name__)
 
@@ -12,7 +15,20 @@ def hello_world():
 @app.route("/predict", methods = ['POST'])
 def predict():
     data = request.get_json()
+
+    channels_amount = data['channels_amount']
+    columns_amount = data['columns_amount']
+    rows_amount = data['rows_amount']
+    pixels = data['pixels']
+    pixels = np.array(list(pixels.values())).reshape((columns_amount, rows_amount, channels_amount)).astype('uint8')
+
+
+    image = Image.fromarray(pixels, mode="RGBA")
+    image = image.resize((28, 28), Image.NEAREST)
     
+    image.convert('RGB')
+    image.save("Downsampled-image.png", "png")
+
     response = {'HELLO': 'from python', 'prediction': 3}
     response = json.dumps(response)
     
